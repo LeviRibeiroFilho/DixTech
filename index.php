2ca65b3d6f887b7b082b5591c3baeae4723a3022
João Vítor <joao.peniza@etec.sp.gov.br>
2021-11-19 11:08:07 -0300

    Adicionando framework Tailwind

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="styles/general.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <title>🏠 - tcm</title>
 
     <!-- Bootstrap Links -->
@@ -22,7 +23,7 @@
 
     <?php 
         include "conexao.php";
-        include "nav.php";
+        include "nav.html";
 
         $consulta = $cn->query('select news_email, news_email_id from news_letter');
         $exibe = $consulta->fetch(PDO::FETCH_ASSOC)
